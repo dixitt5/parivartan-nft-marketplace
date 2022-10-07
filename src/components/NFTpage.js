f2c79bd032733d74f94ecbce62126964aa254532
udayg24 <udaygajera116@gmail.com>
2022-10-07 23:41:20 +0530

    bug fixes

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -86,15 +86,15 @@ export default function NFTPage(props) {
             Seller: <span className="text-sm">{data.seller}</span>
           </div>
           <div>
-            {currAddress == data.owner || currAddress == data.seller ? (
+            {currAddress === data.owner || currAddress === data.seller ? (
+              <div className="text-emerald-700">You are owner of this NFT</div>
+            ) : (
               <button
                 className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
                 onClick={() => buyNFT(tokenId)}
               >
                 Buy this NFT
               </button>
-            ) : (
-              <div className="text-emerald-700">You are the owner of this NFT</div>
             )}
 
             <div className="text-green text-center mt-3">{message}</div>
